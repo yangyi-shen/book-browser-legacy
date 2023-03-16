4bd8f559fa9aa30b51569b5c2eea25289fed4891
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-03-16 15:38:21 +0800

    feat: added web scraping, but not bookstores yet

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const cors = require('cors');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const app = express();
 const PORT = 6900;
 
+//code to allow frontend to read api
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -11,8 +13,33 @@ app.use((req, res, next) => {
     next();
 });
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => {
-    res.send({ username: 'gay jay' })
+
+async function getBWB(query) {
+    try {
+        const escapedQuery = encodeURI(query);
+        const response = await axios.get(`https://news.ycombinator.com`);
+        const $ = cheerio.load(response.data);
+        let pageTitles = '';
+
+        // use cheerio to extract data from response
+        $('.titleline > a').each(function () {
+            pageTitles += (`<h3>${$(this).text()}</h3>`);
+        });
+
+        return pageTitles;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+app.get('/', async (req, res) => {
+    try {
+        const data = await getBWB('amongus');
+        res.send(data);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
 });
 
 app.listen(PORT, () => {
