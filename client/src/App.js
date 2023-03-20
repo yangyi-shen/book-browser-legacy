5732bd7e85724b6c3ccf7a91aa4363886b274111
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-03-20 17:17:15 +0800

    feat: added search functionality using api

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,37 @@ import './App.css';
 
 function App() {
   const [bookList, setBookList] = useState({})
+  const [query, setQuery] = useState('')
 
+  //NOTE: SEARCH api now working. work on putting query inside Searchbar component into fetch() url
+  async function fetchData(query) {
+    await fetch(`http://localhost:6900/api?q=${query}`)
+    .then(response => response.json())
+    .then(booklist => {
+      setBookList(booklist)
+      console.log(booklist.amazonBooks, booklist.bookDepo)
+    })
+    .catch(error => console.error(error))
+  }
+
+  //get search results for trump on load
   useEffect(() => {
-    async function fetchData() {
-      await fetch('http://localhost:6900/api')
-      .then(response => response.json())
-      .then(booklist => {
-        setBookList(booklist)
-        console.log(booklist.amazonBooks, booklist.bookDepo)
-      })
-      .catch(error => console.error(error))
-    }
-
     fetchData();
   }, [])
+
+  function handleChange(event) {
+    setQuery(event.target.value)
+  }
+
+  function handleSubmit() {
+    fetchData(query)
+  }
 
   if (bookList.amazonBooks) {
     return (
       <div className='App'>
+        <input type='text' value={query} onChange={handleChange} />
+        <button type='button' onClick={handleSubmit}>Search</button>
         <h2>Amazon Books:</h2>
         {bookList.amazonBooks.map(book => <p>{book}</p>)}
         <h2>Book Depository:</h2>
