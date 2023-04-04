e617c104456095318ec9eeb50281fb76c735fcfe
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-04-04 11:06:55 +0800

    fix: you can now search by pressing enter key

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -3,10 +3,10 @@ export default function Searchbar(props) {
         <header>
             <h1 id="page-title">Book Browser</h1>
             <h3 id="page-subtitle">Many bookstores, one searchbar</h3>
-            <div id="searchbar">
-                <input id="search-input" type='text' value={props.value} onChange={props.handleChange} placeholder="Find a book.." onkeypress="if(event.keyCode == 13) this.form.submit()" />
-                <button id="search-btn" type='button' onClick={props.handleSubmit}>🔍</button>
-            </div>
+            <form id="searchbar" onSubmit={props.handleSubmit}>
+                <input id="search-input" type='text' value={props.value} onChange={props.handleChange} placeholder="Find a book.." onKeyPress="if(event.keyCode == 13) this.form.submit()" />
+                <button id="search-btn" type='submit'>🔍</button>
+            </form>
         </header>
     )
 }
