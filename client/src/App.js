9663eebf5ab67d8476fd92232448f61fdb36e6c5
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-04-04 11:07:11 +0800

    fix: added loading screen when fetching results

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 
 import { useState, useEffect }from 'react';
 import './App.css';
+import LoadingScreen from './components/LoadingScreen';
 import Searchbar from './components/Searchbar';
 import SearchResult from './components/SearchResult';
 
 function App() {
   const [bookList, setBookList] = useState({})
   const [query, setQuery] = useState('')
+  const [loading, setLoading] = useState(true)
 
   //NOTE: SEARCH api now working. work on putting query inside Searchbar component into fetch() url
   async function fetchData(query) {
+    setLoading(true)
+
     await fetch(`http://localhost:6900/api?q=${query}`)
     .then(response => response.json())
     .then(booklist => {
       setBookList(booklist)
     })
     .catch(error => console.error(error))
+
+    setLoading(false)
   }
 
   //get search results for trump on load
@@ -27,23 +33,22 @@ function App() {
     setQuery(event.target.value)
   }
 
-  function handleSubmit() {
+  function handleSubmit(e) {
+    e.preventDefault()
     fetchData(query)
   }
 
-  if (bookList.bookDepo) {
+  if (loading) {
+    return (
+      <LoadingScreen />
+    )
+  } else {
     return (
       <div className='App'>
         <Searchbar value={query} handleChange={handleChange} handleSubmit={handleSubmit} />
         <main>
           <SearchResult bookList={bookList.allBooks} />
         </main>
-      </div>
-    )
-  } else {
-    return (
-      <div className='App'>
-        <h2>loading...</h2>
       </div>
     )
   }
---
+++ client/src/components/LoadingScreen.js
@@ -0,0 +1,7 @@
+export default function LoadingScreen() {
+    return (
+        <div className='App'>
+            <h2>loading...</h2>
+        </div>
+    )
+}
