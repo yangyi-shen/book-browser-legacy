1d1546e3bf62198e6f9fa64e4eec2bbfb122b605
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-04-04 11:06:20 +0800

    fix: removed main tag in SearchResult component

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -4,8 +4,8 @@ export default function SearchResult(props) {
     const books = props.bookList.map((book, index) => <BookItem key={index} image={book.image} title={book.title} url={book.url} price={book.price} format={book.format} author={book.author} bookstore={book.bookstore} />)
     
     return (
-        <main>
+        <div id="search-result-container">
             {books}
-        </main>
+        </div>
     )
 }
